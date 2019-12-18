bb8b4dfeef375f43d1f79d63f5000c10097c6dd1
Nikita Ermakov <Nikita_Ermakov@epam.com>
2019-12-18 11:59:27 +0300

    Initial code commit

---
+++ src/main/java/com/ermakov/nikita/smokecloud/SmokeCloudApplication.java
@@ -0,0 +1,12 @@
+package com.ermakov.nikita.smokecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokeCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmokeCloudApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ermakov/nikita/smokecloud/SmokeCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ermakov.nikita.smokecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

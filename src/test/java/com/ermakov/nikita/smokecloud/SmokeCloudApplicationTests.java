e61f56f2827751312a35a24995fd13c1d711c6ca
Dependency Injection <xaero@localhost.localdomain>
2019-12-26 22:25:25 +0300

    add failing test

--- src/test/java/com/ermakov/nikita/smokecloud/SmokeCloudApplicationTests.java
+++ src/test/java/com/ermakov/nikita/smokecloud/SmokeCloudApplicationTests.java
@@ -3,11 +3,13 @@ package com.ermakov.nikita.smokecloud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class SmokeCloudApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(false);
 	}
-
 }

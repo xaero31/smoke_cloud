cccce792d99acfa333f2dd07d26717a3fb15ebdd
Nikita Ermakov <Nikita_Ermakov@epam.com>
2020-01-28 10:56:24 +0300

    add kubernetes secrets

--- src/main/java/com/ermakov/nikita/smokecloud/SmokeCloudApplication.java
+++ src/main/java/com/ermakov/nikita/smokecloud/SmokeCloudApplication.java
@@ -2,7 +2,9 @@ package com.ermakov.nikita.smokecloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource(value = "file:/etc/secret/smoke-cloud-secret", ignoreResourceNotFound = true)
 @SpringBootApplication
 public class SmokeCloudApplication {
 

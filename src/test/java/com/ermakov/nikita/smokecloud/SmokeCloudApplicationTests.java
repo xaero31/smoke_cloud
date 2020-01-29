39ad3826f56407d06ae3e82afb1f61ad08cbcf07
Nikita Ermakov <Nikita_Ermakov@epam.com>
2020-01-29 12:13:27 +0300

    add unit tests

--- src/test/java/com/ermakov/nikita/smokecloud/SmokeCloudApplicationTests.java
+++ src/test/java/com/ermakov/nikita/smokecloud/SmokeCloudApplicationTests.java
@@ -1,15 +1,33 @@
 package com.ermakov.nikita.smokecloud;
 
+import com.ermakov.nikita.smokecloud.controller.MainController;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
+@WebMvcTest(controllers = MainController.class)
+@ExtendWith(SpringExtension.class)
 class SmokeCloudApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
-		assertTrue(true);
+	@WithUserDetails("user")
+	void authenticatedMainControllerRequest_ReturnsOk() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/welcome"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	void notAuthenticatedMainControllerRequest_ReturnsNotAuthorized() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/welcome"))
+				.andExpect(MockMvcResultMatchers.status().isUnauthorized());
 	}
 }

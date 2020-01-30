01d9b49a8bd85ddec471fe8945e2e5395543e312
Nikita Ermakov <Nikita_Ermakov@epam.com>
2020-01-30 14:27:27 +0300

    add fake test

--- src/test/java/com/ermakov/nikita/smokecloud/SmokeCloudApplicationTests.java
+++ src/test/java/com/ermakov/nikita/smokecloud/SmokeCloudApplicationTests.java
@@ -1,6 +1,7 @@
 package com.ermakov.nikita.smokecloud;
 
 import com.ermakov.nikita.smokecloud.controller.MainController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.web.servlet.ModelAndView;
 
 @WebMvcTest(controllers = MainController.class)
 @ExtendWith(SpringExtension.class)
@@ -34,5 +37,11 @@ class SmokeCloudApplicationTests {
 	void notAuthenticatedMainControllerRequest_ReturnsNotAuthorized() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/welcome"))
 				.andExpect(MockMvcResultMatchers.status().isUnauthorized());
+	}
+
+	@Test
+	void stubTest() {
+		MainController controller = new MainController();
+		Assertions.assertEquals(controller.getWelcomePage(new ExtendedModelMap()), "index");
 	}
 }

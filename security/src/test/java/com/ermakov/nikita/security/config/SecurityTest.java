93983ba6a090518ef75a5002a27723d4d6fa0484
Dependency Injection <xaero@localhost.localdomain>
2020-02-15 15:33:33 +0300

    fix gradle builds

--- security/src/test/java/com/ermakov/nikita/security/config/SecurityTest.java
+++ security/src/test/java/com/ermakov/nikita/security/config/SecurityTest.java
@@ -2,15 +2,12 @@ package com.ermakov.nikita.security.config;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * created by Nikita_Ermakov at 2/14/2020
  */
-@ExtendWith(MockitoExtension.class)
 public class SecurityTest {
 
     private SecurityConfig config;

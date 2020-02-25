3d82f63790c4aba92587b300816308efd5ae217f
Nikita Ermakov <Nikita_Ermakov@epam.com>
2020-02-25 14:35:23 +0300

    Add token entity

---
+++ model/src/main/java/com/ermakov/nikita/entity/profile/VerificationToken.java
@@ -0,0 +1,31 @@
+package com.ermakov.nikita.entity.profile;
+
+import com.ermakov.nikita.entity.security.User;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * created by Nikita_Ermakov at 2/25/2020
+ */
+@Data
+@Entity
+@Table(name = "verification_token")
+public class VerificationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false, updatable = false)
+    private long id;
+
+    @Column(name = "token", unique = true, nullable = false, updatable = false)
+    private String token;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    @Column(name = "expiration_date", nullable = false, updatable = false)
+    private Date expirationDate;
+}

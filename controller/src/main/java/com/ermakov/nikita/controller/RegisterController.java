d378e790bd9ec019dc0587865fca65efb7757ad9
xaero31 <fontankrovi777@gmail.com>
2020-02-28 21:20:21 +0300

    Fix transactional issue

--- controller/src/main/java/com/ermakov/nikita/controller/RegisterController.java
+++ controller/src/main/java/com/ermakov/nikita/controller/RegisterController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.time.Instant;
 import java.util.Collections;
@@ -63,7 +62,6 @@ public class RegisterController {
         return ViewName.REGISTER;
     }
 
-    @Transactional
     @RequestMapping(path = ControllerPath.REGISTER, method = RequestMethod.POST)
     public String registerPerform(@ModelAttribute @Valid RegisterForm registerForm,
                                   BindingResult result,

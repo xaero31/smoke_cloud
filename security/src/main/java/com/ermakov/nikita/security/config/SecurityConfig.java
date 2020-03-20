338b231673119be6128cc732a4fd7c7d554abe62
Nikita Ermakov <Nikita_Ermakov@epam.com>
2020-03-20 17:20:36 +0300

    Extract html text to message files

--- security/src/main/java/com/ermakov/nikita/security/config/SecurityConfig.java
+++ security/src/main/java/com/ermakov/nikita/security/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/register", "/verifyUser")
+                .antMatchers("/register", "/verifyUser", "/login/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()

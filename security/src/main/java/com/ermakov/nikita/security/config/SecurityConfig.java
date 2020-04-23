dc2606b11dac10ec4c586a41c3df8fd70cf4d247
xaero31 <fontankrovi777@gmail.com>
2020-04-23 18:58:37 +0300

    Fix register bug

--- security/src/main/java/com/ermakov/nikita/security/config/SecurityConfig.java
+++ security/src/main/java/com/ermakov/nikita/security/config/SecurityConfig.java
@@ -19,8 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/register",
-                        "/verifyUser",
+                .antMatchers("/verifyUser",
                         "/login/**",
                         "/register/**",
                         "/css/**")
